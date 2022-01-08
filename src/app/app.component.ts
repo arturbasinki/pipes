b13da3bc3a1ad915a3d3874e7f201cac622780c6
airbass <artur.basinski@gmail.com>
2022-01-08 22:25:44 +0100

    Ready Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = '';
+  date: string = '';
+  amount: number = 0;
+  height: number = 0;
+  miles: number = 0;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000,
+  };
+
+  onNameChange(data: any) {
+    this.name = data.target.value;
+  }
+  onDateChange(data: any) {
+    this.date = data.target.value;
+  }
+  onAmountChange(data: any) {
+    this.amount = parseFloat(data.target.value);
+  }
+  onHeightChange(data: any) {
+    this.height = parseFloat(data.target.value);
+  }
+  onMilesChange(data: any) {
+    this.miles = parseFloat(data.target.value);
+  }
 }
